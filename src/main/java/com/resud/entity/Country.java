1f7eb2fdd617a2893aa4c6a2f1937848de270cb6
Rasuli Ramin <grantik03@gmail.com>
2017-04-20 20:51:43 +0800

    new relationship

---
+++ src/main/java/com/resud/entity/Country.java
@@ -0,0 +1,53 @@
+package com.resud.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by RRM on 20.04.17.
+ */
+@Entity
+public class Country {
+    private int id;
+    private String name;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Country country = (Country) o;
+
+        if (id != country.id) return false;
+        if (name != null ? !name.equals(country.name) : country.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+}
--- src/main/java/com/resud/entity/Student.java
+++ src/main/java/com/resud/entity/Student.java
@@ -4,26 +4,18 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.Collection;
 
+/**
+ * Created by RRM on 20.04.17.
+ */
 @Entity
-@Table(name = "student", schema = "Project")
-public class StudentEntity {
-    private int idStudent;
+public class Student {
     private String firstName;
     private String secondName;
     private String gender;
     private Timestamp birthday;
     private String phone;
     private String address;
-
-    @Id
-    @Column(name = "id_student")
-    public int getIdStudent() {
-        return idStudent;
-    }
-
-    public void setIdStudent(int idStudent) {
-        this.idStudent = idStudent;
-    }
+    private int id;
 
     @Basic
     @Column(name = "first_name")
@@ -85,44 +77,46 @@ public class StudentEntity {
         this.address = address;
     }
 
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        StudentEntity that = (StudentEntity) o;
+        Student student = (Student) o;
 
-        if (idStudent != that.idStudent) return false;
-        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
-        if (secondName != null ? !secondName.equals(that.secondName) : that.secondName != null) return false;
-        if (gender != null ? !gender.equals(that.gender) : that.gender != null) return false;
-        if (birthday != null ? !birthday.equals(that.birthday) : that.birthday != null) return false;
-        if (phone != null ? !phone.equals(that.phone) : that.phone != null) return false;
-        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+        if (id != student.id) return false;
+        if (firstName != null ? !firstName.equals(student.firstName) : student.firstName != null) return false;
+        if (secondName != null ? !secondName.equals(student.secondName) : student.secondName != null) return false;
+        if (gender != null ? !gender.equals(student.gender) : student.gender != null) return false;
+        if (birthday != null ? !birthday.equals(student.birthday) : student.birthday != null) return false;
+        if (phone != null ? !phone.equals(student.phone) : student.phone != null) return false;
+        if (address != null ? !address.equals(student.address) : student.address != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        int result = idStudent;
-        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        int result = firstName != null ? firstName.hashCode() : 0;
         result = 31 * result + (secondName != null ? secondName.hashCode() : 0);
         result = 31 * result + (gender != null ? gender.hashCode() : 0);
         result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
         result = 31 * result + (phone != null ? phone.hashCode() : 0);
         result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + id;
         return result;
     }
 
-    private Collection<CityEntity> id_student;
 
-    @OneToMany(mappedBy = "id_student")
-    public Collection<CityEntity> getId_student() {
-        return id_student;
-    }
 
-    public void setId_student(Collection<CityEntity> id_student) {
-        this.id_student = id_student;
-    }
 }
